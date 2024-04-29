4343e1475fb7d15166df0aed7ad29e538f90dc31
Adlibaari <93728649+Adlibaari@users.noreply.github.com>
2024-04-29 16:21:24 +0700

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,42 @@
 import streamlit as st
 
+def content_based(bookTitle):
+    bookTitle=str(bookTitle)
+    
+    if bookTitle in df["Book-Title"].values:
+        rating_count=pd.DataFrame(df["Book-Title"].value_counts())
+        rare_books=rating_count[rating_count["count"]<=200].index
+        common_books=df[~df["Book-Title"].isin(rare_books)]
+
+        if bookTitle in rare_books:
+            most_common=pd.Series(common_books["Book-Title"].unique()).sample(3).values
+            st.write("No Recommendations for this Book ☹️ \n ")
+            st.write("YOU MAY TRY: \n ")
+            st.write("{}".format(most_common[0]), "\n")
+            st.write("{}".format(most_common[1]), "\n")
+            st.write("{}".format(most_common[2]), "\n")
+        else:
+            common_books=common_books.drop_duplicates(subset=["Book-Title"])
+            common_books.reset_index(inplace=True)
+            common_books["index"]=[i for i in range(common_books.shape[0])]
+            targets=["Book-Title","Book-Author","Publisher"]
+            common_books["all_features"] = [" ".join(common_books[targets].iloc[i,].values) for i in range(common_books[targets].shape[0])]
+            vectorizer=CountVectorizer()
+            common_booksVector=vectorizer.fit_transform(common_books["all_features"])
+            similarity=cosine_similarity(common_booksVector)
+            index=common_books[common_books["Book-Title"]==bookTitle]["index"].values[0]
+            similar_books=list(enumerate(similarity[index]))
+            similar_booksSorted=sorted(similar_books,key=lambda x:x[1],reverse=True)[1:6]
+            books=[]
+            for i in range(len(similar_booksSorted)):
+                
+                books.append(common_books[common_books["index"]==similar_booksSorted[i][0]]["Book-Title"].item())
+                
+            for i in range(len(books)):
+                st.write(books[i], "\n")
+
+    else:
+        st.write("❌ COULD NOT FIND ❌")
+
+st.text_input("Book you want recommendation based off of")
 st.write("Hello")
